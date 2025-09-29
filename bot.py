50a1e36cf390d99cd73b726c99e8831b06dcc5d8
srk451 <seriksisembaev.d@gmail.com>
2025-09-29 17:59:11 +0500

    0.0.1

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# Получаем токен из переменных окружения
+BOT_TOKEN = os.environ.get("8458767187:AAHV6sl14LzVt1Bnk49LvoR6QYg7MAvbYhA")
+
+# Команда /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Привет, {update.effective_user.first_name}! Бот работает ✅")
+
+# Создание приложения
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущен...")
+# Запуск polling
+app.run_polling()
